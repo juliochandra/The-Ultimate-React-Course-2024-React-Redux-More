98f00298d718afdc82f9490ebdb822b15035386b
juliochandra <juliochandra@gmail.com>
2024-02-07 18:40:37 +0700

    update 06-eat-n-split

---
+++ 06-eat-n-split/.prettierrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 75,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  overrides: [
+    {
+      files: "*.js",
+      options: {
+        tabWidth: 5,
+        trailingComma: "all",
+      },
+    },
+    {
+      files: "*.jsx",
+      options: {
+        tabWidth: 3,
+      },
+    },
+    {
+      files: ["*.html", "*.css"],
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: "*.json",
+      options: {
+        printWidth: 80,
+        singleQuote: false,
+        trailingComma: "all",
+        bracketSpacing: false,
+      },
+    },
+  ],
+};
