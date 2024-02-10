1a336b30b1da338aeb925f6e465a6f8a5e272fdf
juliochandra <juliochandra@gmail.com>
2024-02-10 20:15:29 +0700

    how react works

---
+++ 08-how-react-works/.prettierrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none", // Change this line to disable trailing commas
+  printWidth: 75,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  overrides: [
+    {
+      files: "*.js",
+      options: {
+        tabWidth: 3,
+        trailingComma: "none" // Change this line to disable trailing commas
+      }
+    },
+    {
+      files: "*.jsx",
+      options: {
+        tabWidth: 3,
+        trailingComma: "none" // Change this line to disable trailing commas
+      }
+    },
+    {
+      files: ["*.html", "*.css"],
+      options: {
+        tabWidth: 2,
+        trailingComma: "none" // Change this line to disable trailing commas
+      }
+    },
+    {
+      files: "*.json",
+      options: {
+        printWidth: 80,
+        singleQuote: false,
+        trailingComma: "none", // Change this line to disable trailing commas
+        bracketSpacing: false
+      }
+    }
+  ]
+};
